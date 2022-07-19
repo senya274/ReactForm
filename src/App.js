eb70a6ee44e368df9605e77df99d9aa7706f5649
senya274 <senya2470@gmail.com>
2022-07-19 10:23:41 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./index.css";
+import "./styles.css";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="form-signin">
+      <img
+        className="mb-4"
+        src="https://getbootstrap.com/docs/4.5/assets/brand/bootstrap-solid.svg"
+        alt=""
+        width="72"
+        height="72"
+      />
+      <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" className="sr-only">
+        Email address
+      </label>
+      <input
+        type="email"
+        id="inputEmail"
+        className="form-control"
+        placeholder="Email address"
+        required=""
+        autofocus=""
+      />
+      <label for="inputPassword" className="sr-only">
+        Password
+      </label>
+      <input
+        type="password"
+        id="inputPassword"
+        className="form-control"
+        placeholder="Password"
+        required=""
+      />
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <button className="btn btn-lg btn-primary btn-block" type="submit">
+        Sign in
+      </button>
+      <p className="text">2021-2022</p>
+    </form>
   );
 }
-
-export default App;
